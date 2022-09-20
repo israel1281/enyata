0976bc539cc6fec31f61bfc394ab973b1bedc657
Israel__david <kidoski1@gmail.com>
2022-09-20 21:24:10 +0000

    Update 10 files

---
+++ src/views/Login/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AuthLayout from "../../template/authlayout";
+
+export default function Login() {
+  return (
+    <div>
+      <AuthLayout logo={<img src="" alt="" />} form={<img src="" alt="" />} />
+    </div>
+  );
+}
